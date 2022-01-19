400d45beb9e61b1debfe148463901638c7929ba6
jovan0705 <jovan0705@gmail.com>
2022-01-19 21:16:35 +0700

    fix merging sys/loginpage

--- client/src/App.js
+++ client/src/App.js
@@ -1,22 +1,16 @@
 import "./App.css";
 import { Routes, Route } from "react-router-dom";
 import HomePage from "./views/HomePage";
-<<<<<<< HEAD
 import RecipesPage from "./views/RecipesPage";
-=======
 import Login from "./views/Auth/Login"
->>>>>>> sys/loginpage
 
 function App() {
   return (
     <div className="App">
       <Routes>
-<<<<<<< HEAD
         <Route path="/" element={<RecipesPage />} />
-=======
         <Route path="/" element={<HomePage />} />
         <Route path="/login" element={<Login />} />
->>>>>>> sys/loginpage
       </Routes>
     </div>
   );
