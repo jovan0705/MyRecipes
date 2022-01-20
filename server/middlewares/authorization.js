4362f9d5b41cbae1a623b9a41908aa4f6e763046
ihzaw <ihzanantama@gmail.com>
2022-01-21 03:19:50 +0800

    create recipe, edit recipe, delete recipe, create rating

--- server/middlewares/authorization.js
+++ server/middlewares/authorization.js
@@ -6,6 +6,7 @@ const userAuthorization = async (req, res, next) => {
     const userId = req.user.id;
     const userRole = req.user.role;
 
+    // note dari ihza: kayaknya yg dibawah ini parameternya userId bukan targetId
     const user = await User.findByPk(targetId)
         if (!user) {
             throw({name: 'notFound'})
--- server/middlewares/errorHandler.js
+++ server/middlewares/errorHandler.js
@@ -5,7 +5,12 @@ const errorHandler = (err, req, res, next) => {
         case "HEHE":
             res.status(200).json('hehe')
             break;
-    
+        case 'isRated':
+            res.status(400).json({message: 'Item already rated'})
+        case 'badRequest':
+            res.status(400).json({message: 'Bad Request'})
+        case 'notFound':
+            res.status(404).json({message: 'Request Not Found'})
         default:
             res.status(500).json(err)
             break;
