174c05d155fe30a573b7681a888e0e7b59f92292
syswyndar <sysswyndar@gmail.com>
2022-01-23 18:50:09 +0700

    add modal form for add category

--- client/src/views/admin/CategoriesAdmin.jsx
+++ client/src/views/admin/CategoriesAdmin.jsx
@@ -1,12 +1,16 @@
+import { useState } from "react"
 import CategoryList from "../../components/admin/CategoryList"
+import AddCategoryModal from "../forms/AddCategoryModal"
 
 const CategoriesAdmin = () => {
+    const [showModal, setShowModal] = useState(false)
+
     return (
         <>
         <div className="overflow-x-auto">
             <div className="flex justify-between">
                 <h1 className="font-bold text-2xl">Category</h1>
-                <button class="btn btn-outline btn-primary">Add Category</button>
+                <button class="btn btn-outline btn-primary" onClick={() => setShowModal(true)}>Add Category</button>
             </div>
             <table className="table w-full table-zebra border shadow-lg divide-y my-5">
                 <thead>
@@ -22,7 +26,7 @@ const CategoriesAdmin = () => {
                 </tbody>
             </table>
         </div>
-        
+        {showModal && <AddCategoryModal closeModal={setShowModal}/>}
         </>
     )
 }
