13a7186e4c47c4e4e1a071d2059d9624772a94db
syswyndar <sysswyndar@gmail.com>
2022-01-24 09:55:43 +0700

    setup reducer

--- client/src/store/reducers/rootReducer.js
+++ client/src/store/reducers/rootReducer.js
@@ -1,88 +1,12 @@
-import {
-  SET_CLASSES,
-  SET_CLASSES_ERROR,
-  SET_CLASSES_LOADING,
-  SET_CATEGORIES,
-  SET_CATEGORIES_ERROR,
-  SET_CATEGORIES_LOADING,
-  SET_RECIPES,
-  SET_RECIPES_ERROR,
-  SET_RECIPES_LOADING,
-} from "../actionTypes";
+import { combineReducers } from 'redux'
+import recipeReducer from './recipeReducer'
+import categoryReducer from './categoryReducer'
+import classReducer from './classReducer'
 
-const initialState = {
-  recipes: [],
-  recipesError: null,
-  recipesLoading: true,
-  categories: [],
-  categoriesError: null,
-  categoriesLoadig: true,
-  classes: [],
-  classesError: null,
-  classesLoading: true,
-};
+const rootReducer = combineReducers({
+  recipeReducer,
+  categoryReducer,
+  classReducer
+})
 
-export const rootReducer = (state = initialState, { type, payload }) => {
-  switch (type) {
-    //! Recipes
-    case SET_RECIPES:
-      return {
-        ...state,
-        recipes: payload,
-      };
-
-    case SET_RECIPES_ERROR:
-      return {
-        ...state,
-        recipesError: payload,
-      };
-
-    case SET_RECIPES_LOADING:
-      return {
-        ...state,
-        recipesLoading: payload,
-      };
-
-    //! Categories
-    case SET_CATEGORIES:
-      return {
-        ...state,
-        categories: [],
-      };
-
-    case SET_CATEGORIES_ERROR:
-      return {
-        ...state,
-        categoriesError: payload,
-      };
-
-    case SET_CATEGORIES_LOADING:
-      return {
-        ...state,
-        categoriesLoading: payload,
-      };
-
-    //! Classes
-
-    case SET_CLASSES:
-      return {
-        ...state,
-        classes: payload,
-      };
-
-    case SET_CLASSES_ERROR:
-      return {
-        ...state,
-        classesError: payload,
-      };
-
-    case SET_CLASSES_LOADING:
-      return {
-        ...state,
-        classesLoading: payload,
-      };
-
-    default:
-      return state;
-  }
-};
+export default rootReducer
