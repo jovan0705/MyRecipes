f04d7100bbf0eb259c7a9db9ee422f2dd9fd8e95
hwh96 <hwinuratama@gmail.com>
2022-01-26 21:00:57 +0700

    feat: add categories and style tipis di chat

--- server/seeders/20220120123529-categories-seeders.js
+++ server/seeders/20220120123529-categories-seeders.js
@@ -3,21 +3,45 @@
 const data = [
 	{
 		"name" : "Asian",
-		"imageUrl" : "https:\/\/ik.imagekit.io\/blyhh5i9rje\/fried-rice_Z0gnUEkDb.jpg",
-		"createdAt" : "2022-01-20T13:11:00.783Z",
-		"updatedAt" : "2022-01-20T13:11:00.783Z"
+		"createdAt" : "2022-01-26T10:11:04.171Z",
+		"updatedAt" : "2022-01-26T10:11:04.171Z",
+		"imageUrl" : "https:\/\/ik.imagekit.io\/blyhh5i9rje\/cat-asian_Ddc7o_ykunn.jpg"
+	},
+	{
+		"name" : "Italian",
+		"createdAt" : "2022-01-26T10:11:35.977Z",
+		"updatedAt" : "2022-01-26T10:11:35.977Z",
+		"imageUrl" : "https:\/\/ik.imagekit.io\/blyhh5i9rje\/cat-italian_kotTj2MhW.jpg"
 	},
 	{
 		"name" : "Snack",
-		"imageUrl" : "https:\/\/ik.imagekit.io\/blyhh5i9rje\/snack_M7UwSNaiM.jpg",
-		"createdAt" : "2022-01-20T13:13:04.030Z",
-		"updatedAt" : "2022-01-20T13:13:04.030Z"
+		"createdAt" : "2022-01-26T10:11:57.276Z",
+		"updatedAt" : "2022-01-26T10:11:57.276Z",
+		"imageUrl" : "https:\/\/ik.imagekit.io\/blyhh5i9rje\/cat-snack_YpbywS367X8.jpg"
 	},
 	{
-		"name" : "Pasta",
-		"imageUrl" : "https:\/\/ik.imagekit.io\/blyhh5i9rje\/pasta_rz32N9x39.jpg",
-		"createdAt" : "2022-01-20T13:13:54.997Z",
-		"updatedAt" : "2022-01-20T13:15:16.696Z"
+		"name" : "Western",
+		"createdAt" : "2022-01-26T10:12:52.490Z",
+		"updatedAt" : "2022-01-26T10:12:52.490Z",
+		"imageUrl" : "https:\/\/ik.imagekit.io\/blyhh5i9rje\/cat-western_bciI4rXHQwi.jpg"
+	},
+	{
+		"name" : "Japanese",
+		"createdAt" : "2022-01-26T10:13:14.648Z",
+		"updatedAt" : "2022-01-26T10:13:14.648Z",
+		"imageUrl" : "https:\/\/ik.imagekit.io\/blyhh5i9rje\/cat-jap_nbqV6hUmW.jpg"
+	},
+	{
+		"name" : "Chicken",
+		"createdAt" : "2022-01-26T10:13:34.046Z",
+		"updatedAt" : "2022-01-26T10:13:34.046Z",
+		"imageUrl" : "https:\/\/ik.imagekit.io\/blyhh5i9rje\/cat-meat_TV-I2V3up.jpg"
+	},
+	{
+		"name" : "Fruit",
+		"createdAt" : "2022-01-26T10:14:03.042Z",
+		"updatedAt" : "2022-01-26T10:14:03.042Z",
+		"imageUrl" : "https:\/\/ik.imagekit.io\/blyhh5i9rje\/cat-fruit_MeWJVQ0p1.jpg"
 	}
 ]
 
