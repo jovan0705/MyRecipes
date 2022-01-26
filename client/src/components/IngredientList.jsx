62f362c77bc068d45251748c7f389971371a3ecd
Johannes Wijaya <hannesuw@gmail.com>
2022-01-26 09:36:01 +0700

    Commit

--- client/src/components/IngredientList.jsx
+++ client/src/components/IngredientList.jsx
@@ -1,12 +1,16 @@
 import { TiMediaRecord } from "react-icons/ti";
 
-const IngredientList = () => {
-    return (
-        <div className="flex items-center gap-3 text-gray-500">
-            <span className=""><TiMediaRecord/></span>
-            <p><span className="font-bold">200 gr</span> Beef</p>
-        </div>
-    )
-}
+const IngredientList = ({ name }) => {
+  return (
+    <div className="flex items-center gap-3 text-gray-500">
+      <span className="">
+        <TiMediaRecord />
+      </span>
+      <p>
+        <span className="font-bold">{name}</span>
+      </p>
+    </div>
+  );
+};
 
-export default IngredientList
+export default IngredientList;
