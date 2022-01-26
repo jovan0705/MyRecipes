4980143fad8c5030fdd20b3deadbd51ea745b36c
jovan0705 <jovan0705@gmail.com>
2022-01-27 02:18:04 +0700

    fix npm start on heroku

--- client/src/apis/baseUrl.js
+++ client/src/apis/baseUrl.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-const url = "http://localhost:3000";
+const url = "https://my-recipes8.herokuapp.com/";
 
 export const baseUrl = axios.create({
   baseURL: url,
--- client/src/apis/socket.js
+++ client/src/apis/socket.js
@@ -1,3 +1,3 @@
 import { io } from "socket.io-client";
 
-export const socket = io.connect("http://localhost:3000");
+export const socket = io.connect("https://my-recipes8.herokuapp.com/");
